2967ce0c6706927314e24db883e14f5c2a7ab35e
aryanka-pawar-AL <aryanka.pawar@aimedlabs.com>
2023-04-12 17:51:55 +0530

    Whats the similarity between Stream and BehaviorSubject ?

--- Rxdart/b.dart
+++ Rxdart/b.dart
@@ -8,4 +8,8 @@ void main() {
   behaviorSubject.add(1);
   behaviorSubject.add(2);
   behaviorSubject.add(3);
+
+  final numbers = Stream.fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  final oddNumbers = numbers.where((number) => number % 2 != 0);
+  oddNumbers.listen((number) => print(number));
 }
